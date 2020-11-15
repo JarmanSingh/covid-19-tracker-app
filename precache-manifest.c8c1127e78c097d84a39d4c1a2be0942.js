631731e2d28d7fb28faf49d5eefde48c5ec8441a
Jarman Singh <jarmansingh@lidolearning.com>
2020-11-15 18:54:38 +0530

    Updates

---
+++ precache-manifest.c8c1127e78c097d84a39d4c1a2be0942.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f59e874a67b87a26f99118591c478750",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04d73e197e684e18d193",
+    "url": "/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "024a553b03ab2ae29366",
+    "url": "/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "04d73e197e684e18d193",
+    "url": "/static/js/2.ec4b4604.chunk.js"
+  },
+  {
+    "revision": "74abecdc3638247e00d3cef20bdbaf4e",
+    "url": "/static/js/2.ec4b4604.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "024a553b03ab2ae29366",
+    "url": "/static/js/main.f7967a3c.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
